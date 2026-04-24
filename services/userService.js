6ebf7ed0bb6337e7a1aa0de3a567c6798b7a9473
hager ashraf <aab136001@email.com>
2026-04-24 03:12:53 +0300

    add last update

--- services/userService.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/User');
-
-exports.getAllUsers = async () => {
-    return await User.find();
-};
-
-exports.deleteUser = async (id) => {
-    return await User.findByIdAndDelete(id);
-};
